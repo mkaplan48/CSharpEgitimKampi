96bfe056555fa4d180f9a3f2f9b7ceb1e7ba07a8
mkaplan48 <m.kpln48@gmail.com>
2024-10-25 12:34:11 +0300

    ders4

---
+++ 05_LoopWithStars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Alt Alta 10 tane yıldız oluşturma
+            for (int i = 1; i <= 10; i++)
+            {
+            Console.WriteLine("*");
+            
+            }
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
