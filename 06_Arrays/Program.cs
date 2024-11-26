ee038e13b5fe3b19ae29c4a32d9952a507cdf0f2
Muhammet KAPLAN <m.kpln48@gmail.com>
2024-11-26 13:02:34 +0300

    weqr

--- 06_Arrays/Program.cs
+++ 06_Arrays/Program.cs
@@ -53,20 +53,111 @@ namespace _06_Arrays
             //{
             //    Console.WriteLine(colors[i]);
             //}
-            int[] numbers = { 10, 23, 30, 45, 51, 60, 1200, 900, 543, 36, 12, 90, 100, 6754 };
-            for (int i = 0; i < numbers.Length; i++) 
-             {
-                if (numbers[i] % 3 == 0) 
-                { 
-                    Console.WriteLine(numbers[i]);
-                }
-            
-            }
 
+            //ORNEK
+            //int[] numbers = { 10, 23, 30, 45, 51, 60, 1200, 900, 543, 36, 12, 90, 100, 6754 };
+            //for (int i = 0; i < numbers.Length; i++) 
+            // {
+            //    if (numbers[i] % 3 == 0) 
+            //    { 
+            //        Console.WriteLine(numbers[i]);
+            //    }
 
+            //}
+            //
+            #region Örnek:Dizideki Büyük değeri bulma 
+
+            //int[] myArray = { 100, 43, 94589, 35, 9328, 66, 87 };
+            //int MaxNumber = myArray[0];
+            //for (int i = 0; i < myArray.Length; i++)
+            //{
+            //    if (myArray[i] > MaxNumber)
+            //    {
+            //        MaxNumber = myArray[i];
+            //    }
+            //}
+            //    Console.WriteLine(MaxNumber);
 
             #endregion
+            #region Örnek Dizi Uzunluğu bulucu
+            //string[] myArray2 = { "Ali", "Muhammet", "İlay", "Deniz", "Kaplan" };
+            //Console.WriteLine(myArray2.Length);
+            #endregion
+            #region Örnek Sort Metoduyla Sıralama
+            //int[] numbers = { 12, 34, 87, 99, 123, 07, 45, 77, 53, 276 };
+            //Array.Sort(numbers); //Küçükten büyüğe sıraladı
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            #endregion
+            #region Örnek Reverse Metoduyla Diziyi Komple Tersten Sıralama
+
+            //int[] numbers = { 12, 34, 87, 99, 123, 7, 45, 77, 53, 276 };
+            //Array.Reverse(numbers); //Küçükten büyüğe sıraladı
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            #endregion
+            #region Index Of ile dizide arama yapma
+            //string[] customers = { "Buse", "Ali", "İlay", "Deniz", "Şevkinaz" };
+            //int index = Array.IndexOf(customers, "İlay");
+            //Console.WriteLine(index);
+            #endregion
+            #region Dizilerde Max Min Komutları
+            //int[] myArray = { 1, 2, 3, 4, 6, 7, 88, 90, 45, 677 };
+            //Console.WriteLine("Dizinin En Büyük Elemanı: "+myArray.Max()+" "+"Dizinin En Küçük Elemanı: "+myArray.Min());
+
+            #endregion
+            #region  Kullanıcıdan Dizi Değeri Girmesini İsteme
+            //string[] cities = new string[5];
+            //for (int i = 0; i < cities.Length; i++)
+            //{
+            //    Console.Write($"Lütfen {i + 1}. Şehri Giriniz: ");
+            //    cities[i] = Console.ReadLine();
+
+
+            //}
+            //Console.WriteLine("---------------------------------");
+            //for (int j = 0; j < cities.Length; j++) 
+            //{
+            //    Console.WriteLine(cities[j]);
+            //}
+            #endregion
+            #region Dizideki tam sayıları toplayıp yazdırma
+            //int[] Array = { 10, 22, 32, 54, 77, 89, 12, 43, 67, 888, 9 };
+            //int plus = 0;
+            //for (int i=0 ; i<Array.Length; i++)
+            //{
+            //    plus = plus + Array[i];
+            //}
+            //Console.WriteLine(plus);
+            #endregion
+            #region Dizideki tek ve çift sayıları ayrı ayrı listeleme
+            int[] numbers = { 11, 22, 33, 444, 55,76, 67, 81, 93, 140, 141, 612, 413, 214 };
+            Console.WriteLine("Çift Sayılar: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                    {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+            Console.WriteLine("Tek Sayılar: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] %2!=  0)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+
+            #endregion
+            #endregion
             Console.ReadLine();
+            }
         }
     }
-}
+
