6aacdf857183673cde46bec2851370b130f72651
Muhammet KAPLAN <m.kpln48@gmail.com>
2024-12-19 11:18:18 +0300

    ders9 son

--- 09_DatabaseProject/Program.cs
+++ 09_DatabaseProject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +16,7 @@ namespace _09_DatabaseProject
             Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi *****");
             Console.WriteLine();
             Console.WriteLine();
-            string tableNumber
+            string tableNumber;
             Console.WriteLine("--------------------------------------------------------");
             Console.WriteLine("1-Kategoriler");
             Console.WriteLine("2-Ürünler");
@@ -23,8 +25,27 @@ namespace _09_DatabaseProject
             Console.WriteLine("4-Lütfen Getirmek İstediğiniz Tablo Numarasını Giriniz:");
             tableNumber =Console.ReadLine();
             Console.WriteLine("--------------------------------------------------------");
-            
 
+
+            //bu kodların amacı sorguyu ram bellek üzerinden kullancıya gösterebilmek
+            SqlConnection connection = new SqlConnection("Data Source=TIGER;initial " +
+                "Catalog=EgitimKampiDb;integrated security=true");// bağlantı oluşturdum
+            connection.Open();//bağlantıyı açtım 
+            SqlCommand command = new SqlCommand("Select * from TblCategory",connection);//bu tablonunu hangi veri tabanında oluduğunu bildirmek için yanına connection yazıldı
+            SqlDataAdapter adapter = new SqlDataAdapter(command);//benim c# ile veri tabanımın köprü oluşturmasını sağlar
+            DataTable dataTable=new DataTable(); 
+            adapter.Fill(dataTable);
+            foreach (DataRow row in dataTable.Rows)  //birden fazla değişken kullanılacağında var tanımlaması kullanılır.
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                    Console.WriteLine();
+
+            }
+            connection.Close();
+                                            
 
 
 
