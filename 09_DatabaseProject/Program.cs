d1f12a9abeec72a2416ecbe5807b10ee8613973b
Muhammet KAPLAN <m.kpln48@gmail.com>
2024-12-19 10:53:41 +0300

    ders 9

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ado.net
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+            string tableNumber
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.WriteLine("4-Lütfen Getirmek İstediğiniz Tablo Numarasını Giriniz:");
+            tableNumber =Console.ReadLine();
+            Console.WriteLine("--------------------------------------------------------");
+            
+
+
+
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
