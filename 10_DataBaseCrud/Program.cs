3a034ee7828434eaaf3cb644c10cee5ab133b87e
Muhammet KAPLAN <m.kpln48@gmail.com>
2024-12-20 10:46:18 +0300

    ders 10

--- 10_DataBaseCrud/Program.cs
+++ 10_DataBaseCrud/Program.cs
@@ -16,16 +16,40 @@ namespace _10_DataBaseCrud
             Console.WriteLine();
 
             Console.WriteLine("-------------------");
-            Console.WriteLine("Eklemek İstediğiniz Kategori Adı:");
-            string categoryName = Console.ReadLine();
+            #region Kategori Ekleme İşlemi
+            //Console.Write("Eklemek İstediğiniz Kategori Adı:");
+            //string categoryName = Console.ReadLine();
 
-            SqlConnection connection = new SqlConnection("Data Source=TIGER;initial catalog=EgitimKampiDb;integrated security=true");
-            connection.Open();
-            SqlCommand command= new SqlCommand("insert into TblCategory(CategoryName) values (@p1)",connection); 
-            command.Parameters.AddWithValue("@p1",categoryName); 
-            command.ExecuteNonQuery();  
-            connection.Close();
-            Console.WriteLine("Kategori Başarı ile Eklendi!");
+            //SqlConnection connection = new SqlConnection("Data Source=TIGER;initial catalog=EgitimKampiDb;integrated security=true");
+            //connection.Open();
+            //SqlCommand command= new SqlCommand("insert into TblCategory(CategoryName) values (@p1)",connection); 
+            //command.Parameters.AddWithValue("@p1",categoryName); 
+            //command.ExecuteNonQuery();  
+            //connection.Close();
+            //Console.WriteLine("Kategori Başarı ile Eklendi!");
+            #endregion
+            #region Ürün Ekleme İşlemi
+            string eklenecekUrunAdi;
+            decimal eklenecekUrunUcreti;
+            //bool eklenecekUrunDurumu;
+            Console.Write("Eklemek İstediğiniz Yemek Adı:");
+            eklenecekUrunAdi = Console.ReadLine();
+            Console.Write("Eklenecek Ürün Ücreti: ");
+            eklenecekUrunUcreti=decimal.Parse(Console.ReadLine());
+            //Console.Write("Eklenecek Ürünün Stok Durumu: ");
+            // eklenecekUrunDurumu=bool.Parse(Console.ReadLine());
+            SqlConnection baglanti = new SqlConnection("Data source=TIGER;initial catalog=EgitimKampiDb;integrated security=true");
+            baglanti.Open();
+            SqlCommand sqlkomutu = new SqlCommand("insert into TblProduct(ProductName,ProductPrice,ProductStatus) values (@eklenecekUrunAdi,@eklenecekUrunUcreti,@eklenecekUrunDurumu)", baglanti);
+            sqlkomutu.Parameters.AddWithValue("@eklenecekUrunAdi", eklenecekUrunAdi);
+            sqlkomutu.Parameters.AddWithValue("@eklenecekUrunUcreti", eklenecekUrunUcreti);
+            sqlkomutu.Parameters.AddWithValue("@eklenecekUrunDurumu",true);
+            sqlkomutu.ExecuteNonQuery();
+            Console.WriteLine("Ürün Başarıyla Eklendi!");
+
+
+
+            #endregion
             Console.Read();
 
 
