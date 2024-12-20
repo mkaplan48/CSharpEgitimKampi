75ba0ecae71ace8d250c146e984bf7ed8b6fa924
Muhammet KAPLAN <m.kpln48@gmail.com>
2024-12-20 11:30:57 +0300

    ders 10

--- 10_DataBaseCrud/Program.cs
+++ 10_DataBaseCrud/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -29,31 +30,61 @@ namespace _10_DataBaseCrud
             //Console.WriteLine("Kategori Başarı ile Eklendi!");
             #endregion
             #region Ürün Ekleme İşlemi
-            string eklenecekUrunAdi;
-            decimal eklenecekUrunUcreti;
-            //bool eklenecekUrunDurumu;
-            Console.Write("Eklemek İstediğiniz Yemek Adı:");
-            eklenecekUrunAdi = Console.ReadLine();
-            Console.Write("Eklenecek Ürün Ücreti: ");
-            eklenecekUrunUcreti=decimal.Parse(Console.ReadLine());
-            //Console.Write("Eklenecek Ürünün Stok Durumu: ");
-            // eklenecekUrunDurumu=bool.Parse(Console.ReadLine());
-            SqlConnection baglanti = new SqlConnection("Data source=TIGER;initial catalog=EgitimKampiDb;integrated security=true");
-            baglanti.Open();
-            SqlCommand sqlkomutu = new SqlCommand("insert into TblProduct(ProductName,ProductPrice,ProductStatus) values (@eklenecekUrunAdi,@eklenecekUrunUcreti,@eklenecekUrunDurumu)", baglanti);
-            sqlkomutu.Parameters.AddWithValue("@eklenecekUrunAdi", eklenecekUrunAdi);
-            sqlkomutu.Parameters.AddWithValue("@eklenecekUrunUcreti", eklenecekUrunUcreti);
-            sqlkomutu.Parameters.AddWithValue("@eklenecekUrunDurumu",true);
-            sqlkomutu.ExecuteNonQuery();
-            Console.WriteLine("Ürün Başarıyla Eklendi!");
-            baglanti.Close();
-            baglanti.Close();
-            
-           
+            //string eklenecekUrunAdi;
+            //decimal eklenecekUrunUcreti;
+            ////bool eklenecekUrunDurumu;
+            //Console.Write("Eklemek İstediğiniz Yemek Adı:");
+            //eklenecekUrunAdi = Console.ReadLine();
+            //Console.Write("Eklenecek Ürün Ücreti: ");
+            //eklenecekUrunUcreti=decimal.Parse(Console.ReadLine());
+            ////Console.Write("Eklenecek Ürünün Stok Durumu: ");
+            //// eklenecekUrunDurumu=bool.Parse(Console.ReadLine());
+            //SqlConnection baglanti = new SqlConnection("Data source=TIGER;initial catalog=EgitimKampiDb;integrated security=true");
+            //baglanti.Open();
+            //SqlCommand sqlkomutu = new SqlCommand("insert into TblProduct(ProductName,ProductPrice,ProductStatus) values (@eklenecekUrunAdi,@eklenecekUrunUcreti,@eklenecekUrunDurumu)", baglanti);
+            //sqlkomutu.Parameters.AddWithValue("@eklenecekUrunAdi", eklenecekUrunAdi);
+            //sqlkomutu.Parameters.AddWithValue("@eklenecekUrunUcreti", eklenecekUrunUcreti);
+            //sqlkomutu.Parameters.AddWithValue("@eklenecekUrunDurumu",true);
+            //sqlkomutu.ExecuteNonQuery();
+            //Console.WriteLine("Ürün Başarıyla Eklendi!");
+            //baglanti.Close();
+
+
+
 
 
 
             #endregion
+            #region  Ürün Listeleme İşlemi
+            SqlConnection baglanti = new SqlConnection("Data Source=TIGER;initial Catalog=EgitimKampiDb;integrated security=true");
+            baglanti.Open();
+
+            SqlCommand sqlKomutu = new SqlCommand("Select * From TblProduct",baglanti);
+
+            SqlDataAdapter sqlAdapteEt=new SqlDataAdapter(sqlKomutu);//SQL den veriler c# çekecek köprü görevi görür
+            DataTable sqlCekilenVeriTablosu = new DataTable();
+            sqlAdapteEt.Fill(sqlCekilenVeriTablosu);
+
+            foreach(DataRow row in sqlCekilenVeriTablosu.Rows)
+            {
+                foreach(var dr in row.ItemArray)
+                {
+                    Console.WriteLine(dr.ToString());
+                }
+                Console.WriteLine() ;
+            }
+
+
+
+
+
+            baglanti.Close();
+
+
+
+
+            #endregion
+
             Console.Read();
 
 
